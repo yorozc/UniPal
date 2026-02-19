436f72e49dbd0996873863f16a3cabea97802b4a
Yahir Orozco <yorozcolop49@gmail.com>
2026-02-18 22:42:20 -0800

    signup form and getting data

--- src/auth/auth_routes.py
+++ src/auth/auth_routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, url_for
 from . import auth_bp
 from src.data.csu_campuses import CSU_CAMPUSES
 
@@ -13,13 +13,19 @@ def login():
 # create account
 @auth_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == "POST":
+    if request.method == 'POST':
         # check form data
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        major = request.form['major']
+        college = request.form['college']
         
         # get users collection
         # search for existing email
         # if email is found show that it already exists
         # if not found, add to db
-        pass
+        return redirect(url_for('main.index'))
 
     return render_template('signup.html', csu_campuses=CSU_CAMPUSES)
