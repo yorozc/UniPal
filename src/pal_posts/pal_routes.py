c77d1cf574c25fbc3c204640a8f3e8eb89d0e0e8
Yahir Orozco <yorozcolop49@gmail.com>
2026-02-19 12:30:18 -0800

    can create pal posts

--- src/pal_posts/pal_routes.py
+++ src/pal_posts/pal_routes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask_login import current_user
+from bson import ObjectId
 from . import pal_bp
-from src.database.db import get_user_collection
+from src.database.db import get_unipal_posts
 from datetime import datetime
 
 # creating a pal post
@@ -13,11 +15,30 @@ def add_post():
         date = request.form['date']
         time = request.form['time']
         pals = request.form['amount']
+        user_id = ObjectId(current_user.id)
+        coll = get_unipal_posts()
 
         date_obj = datetime.strptime(date, "%Y-%m-%d")
         formatted_date = date_obj.strftime("%m-%d-%Y")
+        time_obj = datetime.strptime(time, "%H:%M")
+        formatted_time = time_obj.strftime("%I:%M %p")
 
-        print(assn_name, desc, class_name, formatted_date, time, pals)
+        post = {
+            "assignment": assn_name,
+            "description": desc,
+            "class": class_name,
+            "start-date": date,
+            "start-time": time,
+            "pals": pals,
+            "user_id": user_id
+        }
+        try:
+            coll.insert_one(post)
+            flash('Posted!', category='success')
+        except Exception as e:
+            flash(f'Error: {e}', category='error')
+
+        print(assn_name, desc, class_name, formatted_date, formatted_time, pals, user_id)
 
     return render_template('pal_form.html')
 
