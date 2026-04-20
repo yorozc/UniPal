8a9aaeb9bd7143e720d3c30948318cc1b6c1e7fe
Yahir Orozco <yorozcolop49@gmail.com>
2026-04-19 22:52:03 -0700

    created return all posts service func

--- src/services/pal_post_service.py
+++ src/services/pal_post_service.py
@@ -1,4 +1,4 @@
-from database.db_conn import get_unipal_posts
+from database.db_conn import palpost_coll
 from models.pal_post import Post, PostWithID
 from bson import ObjectId
 from fastapi import HTTPException
@@ -10,4 +10,10 @@ class PalPostService:
 
     def return_all_posts() -> list[PostWithID]:
         posts = []
+        for post in palpost_coll.find():
+            post["id"] = str(post["_id"])
+            del post["_id"]
+            posts.append(PostWithID(post))
+        return posts
+
         
